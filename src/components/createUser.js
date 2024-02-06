f1ed3b9a761cc96f7a4cea427163ed1191abd31f
vimanshu <vimanshuporiya123@gmail.com>
2024-02-06 21:45:42 +0530

    update user

--- src/components/createUser.js
+++ src/components/createUser.js
@@ -29,7 +29,7 @@ export default class CreateUser extends Component {
 
     console.log(user);
 
-    axios.post(`${baseurl}/exercise/users/add`, user)
+    axios.post(`${baseurl}/users/add`, user)
       .then(res => console.log(res.data));
 
     this.setState({
